ba2fc3bbaebb28112967b674cf573b78c0409412
Johnny <johnnychen@ipevo.com>
2025-10-09 10:17:11 +0800

    core: 專案初始設定，包括 ESLint、Tailwind CSS、Nuxt 配置及開發指南

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,20 @@
+<script setup>
+// 這裡可以加入 Vue 3 Composition API 的邏輯
+</script>
+
 <template>
-  <div>
+  <div class="">
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtLoadingIndicator
+        :throttle="0"
+        color="gray"
+      />
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+
+<style>
+/* 如果需要自定義樣式，可以在這裡加入 */
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,31 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  modules: ['@nuxt/eslint', '@pinia/nuxt'],
+  devtools: {
+    enabled: true,
+  },
+
+  // 禁用自動導入-1： components
+  components: false,
+
+  imports: {
+    autoImport: false, // 禁用自動導入-2： composables 和 utils
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      allowedHosts: true, // 允許所有主機訪問
+    },
+  },
+  eslint: {
+    config: {
+      stylistic: true, // 啟用風格檢查
+    },
+  },
 })
